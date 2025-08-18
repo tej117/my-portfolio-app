dfc991e05f1eedc60702072a5e69f6b6d6d49254
Tejal Cheema <tejalcheema@gmail.com>
2025-08-18 16:04:45 -0700

    Updated Footer with actual information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ const Footer: React.FC = () => {
     return (
         <div className={styles.footer}>
             <div className={styles.contact}>
-                <p>Email: <a href="mailto:your.email@example.com">your.email@example.com</a></p>
-                <p>LinkedIn: <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">Your LinkedIn</a></p>
-                <p>GitHub: <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">Your GitHub</a></p>
+                <p>Email: <a href="mailto:tejalcheema@gmail.com">tejalcheema@gmail.com</a></p>
+                <p>LinkedIn: <a href="www.linkedin.com/in/simran-cheema-690755231" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                <p>GitHub: <a href="https://github.com/tej117" target="_blank" rel="noopener noreferrer">GitHub</a></p>
             </div>
             <div className={styles.footerBottom}>
                 <p>&copy; {new Date().getFullYear()} Simran Cheema </p>
